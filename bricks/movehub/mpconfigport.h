6a94e84fc30c17beb07f04695db89182996c386c
calebjohannas2011-debug <calebjohannas2011@gmail.com>
2026-06-08 18:46:54 +0530

    Replace IO and weak links definitions with SYS

--- bricks/movehub/mpconfigport.h
+++ bricks/movehub/mpconfigport.h
@@ -57,9 +57,9 @@
 #define PYBRICKS_OPT_CUSTOM_IMPORT              (1)
 #define PYBRICKS_OPT_NATIVE_MOD                 (0)
 
-// --- ADD THESE TWO LINES HERE ---
-#define MICROPY_PY_IO                          (1)
-#define MICROPY_PORT_BUILTIN_MODULE_WEAK_LINKS (1)
-// --------------------------------
+// --- REPLACE THE PREVIOUS TWO LINES WITH THESE ---
+#define MICROPY_PY_SYS                          (1)
+#define MICROPY_DECLARE_LEXER_BUILTIN_FILENAME_STR_TABLE_(x)
+// -------------------------------------------------
 
 #include "../_common/mpconfigport.h"
