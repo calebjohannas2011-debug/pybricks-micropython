194c0b55bc1bb7aedf4200b5818d6b6e2c82a425
calebjohannas2011-debug <calebjohannas2011@gmail.com>
2026-07-02 16:05:44 +0530

    Update mpconfigport.h

--- bricks/movehub/mpconfigport.h
+++ bricks/movehub/mpconfigport.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2018-2026 The Pybricks Authors
+// Copyright (c) 2018-2023 The Pybricks Authors
 
 #include "stm32f070xb.h"
 
@@ -12,50 +12,50 @@
 
 // Pybricks modules
 #define PYBRICKS_PY_COMMON                      (1)
-#define PYBRICKS_PY_COMMON_BLE                  (1)
 #define PYBRICKS_PY_COMMON_CHARGER              (0)
 #define PYBRICKS_PY_COMMON_COLOR_LIGHT          (1)
-#define PYBRICKS_PY_COMMON_CONTROL              (0) 
-#define PYBRICKS_PY_COMMON_IMU                  (1) // Required for constructor geometry dependencies
+#define PYBRICKS_PY_COMMON_CONTROL              (0)
+#define PYBRICKS_PY_COMMON_IMU                  (0)
 #define PYBRICKS_PY_COMMON_KEYPAD               (1)
 #define PYBRICKS_PY_COMMON_KEYPAD_HUB_BUTTONS   (1)
-#define PYBRICKS_PY_COMMON_LIGHT_ARRAY          (0)
+#define PYBRICKS_PY_COMMON_LIGHT_ARRAY          (1)
 #define PYBRICKS_PY_COMMON_LIGHT_MATRIX         (0)
-#define PYBRICKS_PY_COMMON_LOGGER               (0) // FORCED OFF: Prevents compiling pb_type_logger.c
+#define PYBRICKS_PY_COMMON_LOGGER               (0)
 #define PYBRICKS_PY_COMMON_MOTOR_MODEL          (0)
 #define PYBRICKS_PY_COMMON_MOTORS               (1)
 #define PYBRICKS_PY_COMMON_SYSTEM               (1)
-#define PYBRICKS_PY_EV3DEVICES                  (0)
 #define PYBRICKS_PY_EXPERIMENTAL                (0)
 #define PYBRICKS_PY_HUBS                        (1)
-#define PYBRICKS_PY_IODEVICES                   (0) // Turned off to prevent cascading logger dependencies
-#define PYBRICKS_PY_NXTDEVICES                  (0)
+#define PYBRICKS_PY_IODEVICES                   (0)
+#define PYBRICKS_PY_MESSAGING                   (1)
+#define PYBRICKS_PY_MESSAGING_APP_DATA          (1)
+#define PYBRICKS_PY_MESSAGING_RFCOMM            (0)
+#define PYBRICKS_PY_MESSAGING_BLE_RADIO         (1)
+#define PYBRICKS_PY_MESSAGING_BLE_RADIO_OLD     (1)
 #define PYBRICKS_PY_PARAMETERS                  (1)
-#define PYBRICKS_PY_PARAMETERS_BUTTON           (1) // Required to fix button compile error
-#define PYBRICKS_PY_PARAMETERS_BUTTON_REMOTE_ONLY (1) 
-#define PYBRICKS_PY_PARAMETERS_GEOMETRY         (1) // Required to fix Axis_Z / Axis_X compile errors
+#define PYBRICKS_PY_PARAMETERS_BUTTON           (1)
+#define PYBRICKS_PY_PARAMETERS_BUTTON_REMOTE_ONLY (1)
 #define PYBRICKS_PY_PARAMETERS_ICON             (0)
 #define PYBRICKS_PY_PARAMETERS_IMAGE            (0)
 #define PYBRICKS_PY_DEVICES                     (1)
 #define PYBRICKS_PY_PUPDEVICES                  (1)
 #define PYBRICKS_PY_PUPDEVICES_DUPLO_TRAIN      (0)
 #define PYBRICKS_PY_PUPDEVICES_MARIO            (0)
-#define PYBRICKS_PY_PUPDEVICES_REMOTE           (0)
+#define PYBRICKS_PY_PUPDEVICES_REMOTE           (1)
 #define PYBRICKS_PY_PUPDEVICES_TECHNIC_MOVE_HUB (0)
-#define PYBRICKS_PY_ROBOTICS                    (0)
+#define PYBRICKS_PY_ROBOTICS                    (1)
 #define PYBRICKS_PY_ROBOTICS_DRIVEBASE_GYRO     (0)
 #define PYBRICKS_PY_ROBOTICS_DRIVEBASE_SPIKE    (0)
-#define PYBRICKS_PY_TOOLS                       (0) // FORCED OFF: Prevents background links to logger
+#define PYBRICKS_PY_TOOLS                       (1)
 #define PYBRICKS_PY_TOOLS_HUB_MENU              (0)
-#define PYBRICKS_PY_TOOLS_APP_DATA              (0)
 
 // Pybricks options
-#define PYBRICKS_OPT_COMPILER                   (1)
+#define PYBRICKS_OPT_COMPILER                   (0)
 #define PYBRICKS_OPT_USE_STACK_END_AS_TOP       (1)
 #define PYBRICKS_OPT_RAW_REPL                   (0)
-#define PYBRICKS_OPT_FLOAT                      (1)
-#define PYBRICKS_OPT_TERSE_ERR                  (1) 
-#define PYBRICKS_OPT_EXTRA_LEVEL1               (1)
+#define PYBRICKS_OPT_FLOAT                      (0)
+#define PYBRICKS_OPT_TERSE_ERR                  (1)
+#define PYBRICKS_OPT_EXTRA_LEVEL1               (0)
 #define PYBRICKS_OPT_EXTRA_LEVEL2               (1)
 #define PYBRICKS_OPT_CUSTOM_IMPORT              (1)
 #define PYBRICKS_OPT_NATIVE_MOD                 (0)
